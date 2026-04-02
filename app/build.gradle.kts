98da56b4efbcc2167b4f8c3bf438894f7388311c
Andebugulin <gulin.andrey2005@gmail.com>
2026-04-02 10:38:26 +0300

    version 1.1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.andebugulin.nfcguard"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.1.5"
+        versionCode = 12
+        versionName = "1.1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
